b0bdfa32176290fcda3be2de786962b167a15b22
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-10-01 19:48:14 +0200

    Created layout for 'PropertyEditFragment'

--- app/src/main/java/com/openclassrooms/realestatemanager/model/Property.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/model/Property.kt
@@ -8,31 +8,21 @@ import java.util.*
 
 data class Property(
         val id:String?=null,
-        var shortDescription:String?=null,
-        var longDescription:String?=null,
+        var adTitle:String?=null,
         var typeId:Int?=null,
         var price:Int?=null,
         var realtorId:String?=null,
+        var description:String?=null,
         var size:Int?=null,
-        var lotSize:Int?=null,
         var nbRooms:Int?=null,
         var nbBedrooms:Int?=null,
         var nbBathrooms:Int?=null,
         var address:String?=null,
+        var postalCode:String?=null,
+        var city:String?=null,
+        var country:String?=null,
+        var buildDate:Date?=null,
         var adDate: Date?=null,
         var saleDate:Date?=null,
-        var buildDate:Date?=null
-        ){
-
-    var sold:Boolean =false ; private set
-
-    /**Second constructor allowing to set a value to 'sold'**/
-
-    constructor(sold:Boolean):this(){this.sold=sold}
-
-    /**Sets 'sold' to true **/
-
-    fun notifyPropertyIsSold(){
-        sold=true
-    }
-}
+        var sold:Boolean=false
+        )
