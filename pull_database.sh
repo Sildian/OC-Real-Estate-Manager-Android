8f7ff589f4fc03afc02c4d90eacc5c7a7e84f478
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-11-06 09:41:36 +0100

    Fixed some issues with gradle

---
+++ pull_database.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'Getting the database'
+/Users/sildian/Library/Android/sdk/platform-tools/adb root
+/Users/sildian/Library/Android/sdk/platform-tools/adb pull /data/data/com.openclassrooms.realestatemanager/databases
