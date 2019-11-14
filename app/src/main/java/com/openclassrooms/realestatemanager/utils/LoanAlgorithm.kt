6af9fd656574173c6fe371f9bccb149650409622
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2019-11-14 19:28:23 +0100

    Feeded 'LoanFragment' with the needed UI components and behaviour

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/LoanAlgorithm.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/LoanAlgorithm.kt
@@ -8,9 +8,20 @@ import kotlin.math.pow
 
 object LoanAlgorithm {
 
+    /**Calculates a loan amount
+     * @param investmentAmount : the amount of the investment
+     * @param contributionAmount : the amount of the contribution from the buyer
+     */
+
     fun calculateLoanAmount(investmentAmount:Long, contributionAmount:Long):Long{
         return investmentAmount-contributionAmount
     }
+
+    /**Calculates a loan's monthly payments
+     * @param loanAmount : the amount of the loan
+     * @param nbYears : the number of years for the loan
+     * @param interestRate : the interest rate
+     */
 
     fun calculateLoanMonthlyPayments(loanAmount:Long, nbYears:Int, interestRate:Double):Double{
         val nbMonths=nbYears*12
--- app/src/main/java/com/openclassrooms/realestatemanager/view/fragments/LoanFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/fragments/LoanFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.utils.LoanAlgorithm
+import kotlinx.android.synthetic.main.fragment_loan.view.*
 
 /**************************************************************************************************
  * Loan simulator for the user
@@ -14,9 +16,72 @@ import com.openclassrooms.realestatemanager.R
 
 class LoanFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loan, container, false)
+    /*********************************************************************************************
+     * UI components
+     ********************************************************************************************/
+
+    /**Main layout**/
+
+    private lateinit var layout:View
+
+    /**Text input layouts**/
+
+    private val investmentAmountLayout by lazy {layout.fragment_loan_investment_layout}
+    private val contributionAmountLayout by lazy {layout.fragment_loan_contribution_layout}
+    private val durationLayout by lazy {layout.fragment_loan_duration_layout}
+    private val interestRateLayout by lazy {layout.fragment_loan_interest_rate_layout}
+
+    /**Components on the screen**/
+
+    private val investmentAmount by lazy {layout.fragment_loan_investment}
+    private val contributionAmount by lazy {layout.fragment_loan_contribution}
+    private val loanAmount by lazy {layout.fragment_loan_loan}
+    private val duration by lazy {layout.fragment_loan_duration}
+    private val interestRate by lazy {layout.fragment_loan_interest_rate}
+    private val monthlyPayments by lazy {layout.fragment_loan_monthly_payments}
+    private val calculateButton by lazy {layout.fragment_loan_button_calculate}
+
+    /*********************************************************************************************
+     * Life cycle
+     ********************************************************************************************/
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        this.layout=inflater.inflate(R.layout.fragment_loan, container, false)
+        initializeCalculateButton()
+        return this.layout
+    }
+
+    /*********************************************************************************************
+     * UI initialization
+     ********************************************************************************************/
+
+    private fun initializeCalculateButton(){
+        this.calculateButton.setOnClickListener { calculateLoanAmounts() }
+    }
+
+    /*********************************************************************************************
+     * Loan calculation
+     ********************************************************************************************/
+
+    private fun calculateLoanAmounts(){
+        val loanAmount=calculateLoanAmount()
+        calculateMonthlyPaymentsAmount(loanAmount)
+    }
+
+    private fun calculateLoanAmount():Long{
+        val investmentAmount:Long=this.investmentAmount.text.toString().toLong()
+        val contributionAmount:Long=this.contributionAmount.text.toString().toLong()
+        val result=LoanAlgorithm.calculateLoanAmount(investmentAmount, contributionAmount)
+        val loanAmountToDisplay=resources.getString(R.string.label_loan_loan)+" : "+result
+        this.loanAmount.text=loanAmountToDisplay
+        return result
+    }
+
+    private fun calculateMonthlyPaymentsAmount(loanAmount:Long){
+        val duration:Int=this.duration.text.toString().toInt()
+        val interestRate:Double=this.interestRate.text.toString().toDouble()
+        val result=LoanAlgorithm.calculateLoanMonthlyPayments(loanAmount, duration, interestRate)
+        val monthlyPaymentsToDisplay=resources.getString(R.string.label_loan_monthly_payments)+" : "+result
+        this.monthlyPayments.text=monthlyPaymentsToDisplay
     }
 }
